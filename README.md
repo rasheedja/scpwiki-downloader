1effc20f7ceeff448e37d3a900f18fcef382a67b
Junaid Ali Rasheed <jrasheed178@gmail.com>
2015-07-26 18:49:14 +0100

    Version 1.01
    Ported to Python 3.4.

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # scpwiki-downloader
 Downloads every existing SCP page from www.scp-wiki.net
 
-To use this, install python 2.7 and run SCPWikiDownloader.py. This script takes
+To use this, install python 3.4 and run SCPWikiDownloader.py. This script takes
 some time to run as it will download every SCP page from 001 to 2999.
--- SCPWikiDownloader.py
+++ SCPWikiDownloader.py
@@ -1,15 +1,16 @@
 # Downloads every scp page at www.scp-wiki.net
 # and stores every page in an archive.
 import urllib
-import urllib2
+import urllib.error
+import urllib.request
 import os
 import zipfile
-import time
+my_zip = zipfile.ZipFile('SCPWiki.zip', 'w', zipfile.ZIP_DEFLATED)
 
 
 def save_wiki(i):
 
-    while i < 3000:
+    while i < 3000:  # The wiki has up to 2999 SCPs as of 26/07/2015.
         if i < 10:
             url = "http://www.scp-wiki.net/scp-00"
             page = "scp-00"
@@ -26,22 +27,19 @@ def save_wiki(i):
             url += str(i)
             page += str(i)
         try:
+
             status = "Downloading "
             status += page
-            print status
-            scp_page = urllib2.urlopen(url)
-            scp_page_content = scp_page.read()
-        except urllib2.HTTPError, e:
-            print "Download failed, page does not exist"
+            print(status)
+            page += '.html'
+            urllib.request.urlretrieve(url, page)  # Download and save page as scp-x.html.
+        except urllib.error.URLError:
+            print("Download failed, page does not exist")
             i += 1
-            save_wiki(i)
+            save_wiki(i)  # If page does not exist, move on to next page.
         else:
-            print "Download Successful"
-        page += '.html'
-        with open(page, 'w') as foo:
-            foo.write(scp_page_content)
+            print("Download Successful")
         i += 1
         my_zip.write(page)
         os.remove(page)
-my_zip = zipfile.ZipFile('SCPWiki.zip', 'w', zipfile.ZIP_DEFLATED)
 save_wiki(1)
